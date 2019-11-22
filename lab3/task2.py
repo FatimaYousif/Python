9b7e866cc055bcaee08b47ca57dbbd681607c17d
FatimaYousif <49322171+FatimaYousif@users.noreply.github.com>
2019-11-22 18:22:47 +0300

    Add files via upload
    I already did upload these tasks earlier on 12 AM today, with each separate .py files and here I just stored them in a separate folder of lab3 (assignment 3).

---
+++ lab3/task2.py
@@ -0,0 +1,8 @@
+#task2
+
+a=[1,2,3,4,5]
+num=int(input("enter num= "))
+for numb in a :
+    if numb==num:
+        print("number is there")
+        
---
+++ lab3/task3.py
@@ -0,0 +1,10 @@
+#task 3
+
+a={"name": "fatima"}
+
+#adding an item in the dictionary
+a["r_no"]=29;
+
+#printing them
+print(a["name"])
+print(a["r_no"])
