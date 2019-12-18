2a1e57bb2260733979e4596abdb5866b0896190f
FatimaYousif <49322171+FatimaYousif@users.noreply.github.com>
2019-12-18 17:26:25 +0300

    Update task1.py

--- lab 5/task1.py
+++ lab 5/task1.py
@@ -1,8 +1,7 @@
 #task 1
 
 def factorial(n) :
-    if(n==1):
-        return n
-    else:
-        return n*factorial(n-1)
-factorial(int(5))
+    fact=1;
+    for i in range (1,n+1):
+        fact*=i
+factorial(5)
